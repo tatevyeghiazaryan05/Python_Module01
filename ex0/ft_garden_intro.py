344bd15681b801861b0c50f01628359ee3a94174
root <root@DESKTOP-63BM6S7.>
2026-06-23 21:44:48 +0400

    Ex0 ex1 are donegit add .

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro() -> None:
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("\n=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
